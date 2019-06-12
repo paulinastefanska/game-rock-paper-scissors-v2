0d7f7cf0ffece16f71194339ba3d7ce50ceb240b
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-12 19:26:13 +0200

    Add variables

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var rock = document.getElementById("rockButton");
+var paper = document.getElementById("paperButton");
+var scissors = document.getElementById("scissorsButton");
+var outputMessages = document.getElementById("outputResult");
+var newGameButton = document.getElementById("newGameBtn");
+var playerMove = document.getElementById("playerMove");
+var playerScore = document.getElementById("playerScore");
+var cpuScore = document.getElementById("cpuScore");
+var modals = document.querySelectorAll(".modal");
+var playerNameInput = document.querySelector("#player-name");
+var roundsNumberInput = document.querySelector("#rounds-number");
+var startGamePopupButton = document.querySelector("#start-button");
