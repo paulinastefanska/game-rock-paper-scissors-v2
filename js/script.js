dc4da58462673d7c846f928a38e7e37f118b0f3b
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-12 19:36:42 +0200

    Add showModal and hideModal

--- js/script.js
+++ js/script.js
@@ -12,3 +12,44 @@ var modals = document.querySelectorAll(".modal");
 var playerNameInput = document.querySelector("#player-name");
 var roundsNumberInput = document.querySelector("#rounds-number");
 var startGamePopupButton = document.querySelector("#start-button");
+
+var params = {
+  victories: 0,
+  playerPoints: 0,
+  cpuPoints: 0,
+  rounds: 0,
+  message: "",
+  progress: []
+};
+
+var gameProgress = {
+  roundNumber: 0,
+  message: "",
+  result: ""
+};
+
+var showModal = function showModal(modalId) {
+  document.querySelector("#modal-overlay").classList.add("show");
+  modals.forEach(function (modal) {
+    modal.classList.remove("show");
+  });
+  document.querySelector(modalId).classList.add("show");
+};
+
+var hideModal = function hideModal() {
+  document.querySelector("#modal-overlay").classList.remove("show");
+};
+
+document.querySelector("#modal-overlay").addEventListener("click", hideModal);
+
+for (var i = 0; i < modals.length; i++) {
+  modals[i].addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+}
+
+var closeButtons = document.querySelectorAll(".modalClose");
+
+for (var j = 0; j < closeButtons.length; j++) {
+  closeButtons[j].addEventListener("click", hideModal);
+}
